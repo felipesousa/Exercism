3c642dbc2769966c320355f442a2913b301de339
Rob Prouse <rob@prouse.org>
2014-09-25 16:06:07 -0400

    Setup for the Nucleotide Count exercise

---
+++ nucleotide-count/DNA.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class DNA
+{
+    /// <summary>
+    /// Constructs the DNA
+    /// </summary>
+    /// <param name="dnaString">The string representation of the nucleotides</param>
+    public DNA(string dnaString)
+    {
+        if(dnaString == null) throw new ArgumentNullException("dnaString");        
+    }
+
+    /// <summary>
+    /// Gets a list of the nucleotide counts
+    /// </summary>
+    public IDictionary<char, int> NucleotideCounts { get; private set; }
+    
+    /// <summary>
+    /// Gets the count of a particular nucleotide
+    /// </summary>
+    /// <param name="nucleotide">The nucleotide to count</param>
+    /// <returns>The number of this nucleotide in this DNA</returns>
+    public uint Count(char nucleotide)
+    {
+        return 0;
+    }
+}
+
+/// <summary>
+/// The exception thrown when requesting a count of an invalid nucleotide
+/// </summary>
+public class InvalidNucleotideException : Exception
+{
+}
