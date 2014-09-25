4af5a731b23af484a59b685c72703914cfe799a4
Rob Prouse <rob@prouse.org>
2014-09-25 13:58:07 -0400

    Setup for the WordCount exercise

---
+++ word-count/Phrase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Phrase
+{
+    private readonly string _phrase;
+
+    public Phrase(string phrase)
+    {
+        _phrase = phrase;
+    }
+
+    /// <summary>
+    /// Gets a word count on this phrase
+    /// </summary>
+    /// <returns>The word count</returns>
+    public Dictionary<string, int> WordCount()
+    {
+        var counts = new Dictionary<string, int>();
+
+        return counts;
+    }
+}
