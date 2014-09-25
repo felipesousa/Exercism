6b48c29de31e74d8f2fe759e5156f4f7afae4dd0
Rob Prouse <rob@prouse.org>
2014-09-25 16:59:19 -0400

    Solution for the DNA nucleotide exercise

--- nucleotide-count/DNA.cs
+++ nucleotide-count/DNA.cs
@@ -1,24 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 public class DNA
 {
+    private const char ADENINE  = 'A';
+    private const char CYTOSINE = 'C';
+    private const char GUANINE  = 'G';
+    private const char THYMINE  = 'T';
+    private static char[] NUCLEOTIDES = new[] {
+        ADENINE, CYTOSINE, GUANINE, THYMINE
+    };
+
     /// <summary>
     /// Constructs the DNA
     /// </summary>
     /// <param name="dnaString">The string representation of the nucleotides</param>
     public DNA(string dnaString)
     {
-        if(dnaString == null) throw new ArgumentNullException("dnaString");        
+        if(dnaString == null) throw new ArgumentNullException("dnaString");
+        Initialize(dnaString);
     }
 
     /// <summary>
     /// Gets a list of the nucleotide counts
     /// </summary>
-    public IDictionary<char, int> NucleotideCounts { get; private set; }
+    public ImmutableDictionary<char, uint> NucleotideCounts { get; private set; }
     
     /// <summary>
     /// Gets the count of a particular nucleotide
@@ -27,7 +37,34 @@ public class DNA
     /// <returns>The number of this nucleotide in this DNA</returns>
     public uint Count(char nucleotide)
     {
-        return 0;
+        nucleotide = ValididateNucleotide(nucleotide);
+        if(!NucleotideCounts.ContainsKey(nucleotide))
+            return 0;
+        return NucleotideCounts[nucleotide];
+    }
+
+    private void Initialize(string dnaString)
+    {
+        var counts = ImmutableDictionary<char, uint>.Empty.ToBuilder();
+
+        foreach(char nucleotide in NUCLEOTIDES)
+            counts[nucleotide] = 0;
+
+        foreach(char nucleotide in dnaString.Select(c => ValididateNucleotide(c)))
+            counts[nucleotide]++;
+
+        NucleotideCounts = counts.ToImmutable();
+    }
+
+    private char ValididateNucleotide(char nucleotide)
+    {
+        nucleotide = (from n in NUCLEOTIDES
+                      where n == Char.ToUpper(nucleotide)
+                      select n).FirstOrDefault();
+
+        if(nucleotide == default(char))
+            throw new InvalidNucleotideException(nucleotide);
+        return nucleotide;
     }
 }
 
@@ -36,4 +73,13 @@ public class DNA
 /// </summary>
 public class InvalidNucleotideException : Exception
 {
+    public InvalidNucleotideException()
+        : base("Invalid nucleotide")
+    {
+    }
+
+    public InvalidNucleotideException(char nucleotide) 
+        : base(string.Format("{0} is not a valid nucleotide", nucleotide))
+    {
+    }
 }
