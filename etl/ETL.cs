2eed4fc6596e4964b34a162e2fe9a7ffa80bf680
Rob Prouse <rob@prouse.org>
2014-09-24 22:40:57 -0400

    Setup for exerise 3

---
+++ etl/ETL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class ETL
+{
+    public static Dictionary<string, int> Transform(Dictionary<int, IList<string>> input)
+    {
+        return null;
+    }
+}
