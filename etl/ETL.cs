e52c5817e3ec8aaa5e7c1e8fdb8a51b893d42aa4
Rob Prouse <rob@prouse.org>
2014-09-25 11:34:02 -0400

    ETL unit tests passing

--- etl/ETL.cs
+++ etl/ETL.cs
@@ -6,8 +6,21 @@ using System.Threading.Tasks;
 
 public static class ETL
 {
+    /// <summary>
+    /// Transforms from the old scoring format to the new format
+    /// </summary>
+    /// <param name="input">The old data format</param>
+    /// <returns>The new data format</returns>
     public static Dictionary<string, int> Transform(Dictionary<int, IList<string>> input)
     {
-        return null;
+        var output = new Dictionary<string, int>();
+        foreach(var pair in input)
+        {
+            foreach(var letter in pair.Value)
+            {
+                output[letter.ToLower()] = pair.Key;
+            }
+        }
+        return output;
     }
 }
