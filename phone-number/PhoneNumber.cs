4bb2475b6d6c9614b3fced434e6d20dab840fdb6
Rob Prouse <rob@prouse.org>
2014-09-26 14:42:59 -0400

    Switched the phone number parsing to a Regex

--- phone-number/PhoneNumber.cs
+++ phone-number/PhoneNumber.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 /// <summary>
@@ -9,36 +10,37 @@ using System.Threading.Tasks;
 /// </summary>
 public class PhoneNumber
 {
+    private const string KILLER_REGEX = @"^(?:1|[^\d]*)(?<AreaCode>\d{3})(?:[^\d]*)(?<ExchangeCode>\d{3})(?:[^\d]*)(?<SubscriberNumber>\d{4})$";
     private const string NUMBER_FORMAT  = "({0}) {1}-{2}";
-    private const string DEFAULT_NUMBER = "0000000000";
+
     /// <summary>
     /// Constructs a phone number from a string
     /// </summary>
     /// <param name="number">A string representation of a phone number</param>
     public PhoneNumber(string number)
     {
-        CleanPhoneNumber(number);
+        ParsePhoneNumber(number);
     }
 
     /// <summary>
     /// The sanitized version of the phone number.
     /// </summary>
-    public string Number { get; private set; }
+    public string Number { get { return string.Concat(AreaCode, ExchangeCode, SubscriberNumber); } }
 
     /// <summary>
     /// Gets the area code portion of the phone number
     /// </summary>
-    public string AreaCode { get { return Number.Substring(0, 3); } }
+    public string AreaCode { get; private set; }
 
     /// <summary>
     /// Gets the exchange code portion (second group of three digits) of the phone number.
     /// </summary>
-    public string ExchangeCode { get { return Number.Substring(3, 3); } }
+    public string ExchangeCode { get; private set; }
 
     /// <summary>
     /// Gets the subscriber number portion (last four digits) of the phone number.
     /// </summary>
-    public string SubscriberNumber { get { return Number.Substring(6); } }
+    public string SubscriberNumber { get; private set; }
 
     /// <summary>
     /// Returns a <see cref="System.String" /> that represents this Phone number.
@@ -51,22 +53,20 @@ public class PhoneNumber
         return string.Format(NUMBER_FORMAT, AreaCode, ExchangeCode, SubscriberNumber);
     }
 
-    private void CleanPhoneNumber(string number)
+    private void ParsePhoneNumber(string number)
     {
-        // Strip everything but numbers
-        var digits = number.Where(c => char.IsDigit(c)).ToArray();
-        var clean = new string(digits);
-        if (clean.Length == 10)
+        Match match = Regex.Match(number, KILLER_REGEX);
+        if (match.Success)
         {
-            Number = clean;
-        }
-        else if(clean.Length == 11 && clean[0] == '1')
-        {
-            Number = clean.Substring(1);
+            AreaCode         = match.Groups["AreaCode"].Value;
+            ExchangeCode     = match.Groups["ExchangeCode"].Value;
+            SubscriberNumber = match.Groups["SubscriberNumber"].Value;
         }
         else
         {
-            Number = DEFAULT_NUMBER;
+            AreaCode         = "000";
+            ExchangeCode     = "000";
+            SubscriberNumber = "0000";
         }
     }
 }
