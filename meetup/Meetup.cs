e8d174c0c4b69f21045dfbf69ac8f6b75ea23ebf
Rob Prouse <rob@prouse.org>
2014-09-27 09:37:37 -0400

    Setup for the meetup exercise

---
+++ meetup/Meetup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Meetup
+{
+    public Meetup(int month, int year)
+    {
+    }
+
+    public DateTime Day(DayOfWeek dayOfWeek, Schedule schedule)
+    {
+        return DateTime.Now;
+    }
+}
+
+public enum Schedule
+{
+    First,
+    Second,
+    Third,
+    Fourth,
+    Last,
+    Teenth
+}
