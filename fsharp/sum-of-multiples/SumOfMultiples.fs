a14a2d00981894dcf8dcaf3562451097895f5266
Rob Prouse <rob@prouse.org>
2014-09-30 10:50:54 -0400

    Sum of multiples exercise in F#

---
+++ fsharp/sum-of-multiples/SumOfMultiples.fs
@@ -0,0 +1,20 @@
+﻿module SumOfMultiples
+
+open System.Collections.Generic
+open System.Linq
+
+type SumOfMultiples = 
+    val baseMultiples : IEnumerable<int>
+
+    new () = { baseMultiples = [3; 5] }
+
+    new baseMultiples = { baseMultiples = baseMultiples }
+
+    member this.To value = 
+        this.multiplesOf(value) |> Seq.sum
+
+    member private this.multiplesOf value:seq<int> =
+        seq<int> { for i in 1..(value - 1) do if this.ismultiple i then yield i }
+
+    member private this.ismultiple value =
+        this.baseMultiples.Any(fun baseMultiple -> value % baseMultiple = 0)
