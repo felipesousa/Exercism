d1b2e5e3ba304353aae384073eb2612a565c6c23
Rob Prouse <rob@prouse.org>
2014-09-30 11:51:51 -0400

    Making it a bit more F#'y

--- fsharp/sum-of-multiples/SumOfMultiples.fs
+++ fsharp/sum-of-multiples/SumOfMultiples.fs
@@ -3,18 +3,13 @@
 open System.Collections.Generic
 open System.Linq
 
-type SumOfMultiples = 
-    val baseMultiples : IEnumerable<int>
+type SumOfMultiples(baseMultiples) =         
 
-    new () = { baseMultiples = [3; 5] }
+    member s.To value = 
+        let multiples = seq { for i in 1..(value - 1) do if s.multipleOf i then yield i }
+        multiples |> Seq.sum
+    
+    member s.multipleOf value =
+        baseMultiples |> Seq.exists( fun multiple -> value % multiple = 0 )
 
-    new baseMultiples = { baseMultiples = baseMultiples }
-
-    member this.To value = 
-        this.multiplesOf(value) |> Seq.sum
-
-    member private this.multiplesOf value:seq<int> =
-        seq<int> { for i in 1..(value - 1) do if this.ismultiple i then yield i }
-
-    member private this.ismultiple value =
-        this.baseMultiples.Any(fun baseMultiple -> value % baseMultiple = 0)
+    new () = SumOfMultiples([3; 5])
