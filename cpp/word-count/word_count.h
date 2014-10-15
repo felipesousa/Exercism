1d089797d96621c018f7ceffd47c04970e60a265
Rob Prouse <rob@prouse.org>
2014-10-15 15:57:48 -0400

    Switched to using the regex in the std library

--- cpp/word-count/word_count.h
+++ cpp/word-count/word_count.h
@@ -1,19 +1,21 @@
 #pragma once
 #include <string>
 #include <map>
-#include <boost/regex.hpp>
-#include <boost/algorithm/string.hpp>
+#include <algorithm>
+#include <iterator>
+#include <regex>
+#include <boost/algorithm/string/case_conv.hpp>
 
 namespace word_count
 {
     std::map<std::string, int> words( const std::string& sentence )
     {
-        static const boost::regex regex( "\\w+'\\w+|\\w+" );
+        static const std::regex regex( "\\w+'\\w+|\\w+" );
         std::map<std::string, int> count;
         std::string normalized = boost::to_lower_copy( sentence );
 
-        boost::sregex_token_iterator word( normalized.begin(), normalized.end(), regex );
-        boost::sregex_token_iterator end;
+        std::sregex_token_iterator word( normalized.begin(), normalized.end(), regex );
+        std::sregex_token_iterator end;
         while ( word != end )
         {
             count[*word++]++;
