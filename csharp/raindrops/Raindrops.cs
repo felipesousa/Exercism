f4e779d4e54e7efba182ec60bc3d5e8d806b3208
Rob Prouse <rob@prouse.org>
2014-10-16 13:48:53 -0400

    Worked a new algorithm for the raindrop exercise

--- csharp/raindrops/Raindrops.cs
+++ csharp/raindrops/Raindrops.cs
@@ -1,33 +1,20 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 
 public static class Raindrops
 {
-    private static readonly IDictionary<int, string> rain = 
-        new Dictionary<int, string>
+    private static readonly Tuple<int, string>[] rain =
         {
-            { 3, "Pling" }, { 5, "Plang" }, { 7, "Plong" }
+            new Tuple<int, string>( 3, "Pling" ), 
+            new Tuple<int, string>( 5, "Plang" ), 
+            new Tuple<int, string>( 7, "Plong" )
         };
 
     public static string Convert(int number)
     {
-        var words = PrimeFactors(number)
-                        .Where(n => rain.ContainsKey(n))
-                        .Distinct()
-                        .Select(i => rain[i])
-                        .ToArray();
+        var words = (from pair in rain 
+                     where number % pair.Item1 == 0 
+                     select pair.Item2).ToArray();
         return words.Any() ? String.Concat(words) : number.ToString();
-    }
-
-    private static IEnumerable<int> PrimeFactors(long i)
-    {
-        int factor = 2;
-        while(i > 1)
-        {
-            while(i % factor != 0) factor++;
-            i /= factor;
-            yield return factor;
-        }
     }
 }
