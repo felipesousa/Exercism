48af445e6dbb50d353ecff841556c97d2ad79976
Rob Prouse <rob@prouse.org>
2014-11-08 15:47:22 -0500

    Simple solution for a simple problem

---
+++ csharp/accumulate/Accumulate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class AccumulateExtensions
+{
+    public static IEnumerable<T> Accumulate<T>(this IEnumerable<T> source, Func<T, T> operation )
+    {
+        foreach (var item in source)
+        {
+            yield return operation(item);
+        }
+    }
+}
