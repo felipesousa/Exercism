e1ee26514bfb376427545ebaa0e05018ccaee47e
Rob Prouse <rob@prouse.org>
2014-11-08 15:52:29 -0500

    A more robust solution

--- csharp/accumulate/Accumulate.cs
+++ csharp/accumulate/Accumulate.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 public static class AccumulateExtensions
 {
-    public static IEnumerable<T> Accumulate<T>(this IEnumerable<T> source, Func<T, T> operation )
+    public static IEnumerable<T2> Accumulate<T1, T2>(this IEnumerable<T1> source, Func<T1, T2> operation )
     {
         foreach (var item in source)
         {
