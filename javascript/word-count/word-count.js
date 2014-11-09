67f6ce6164e5501f1cbfaf9b49d7fc836599e7d0
Rob Prouse <rob@prouse.org>
2014-11-08 21:47:12 -0500

    solution for the javascript word-count

---
+++ javascript/word-count/word-count.js
@@ -0,0 +1,9 @@
+﻿'use strict';
+
+module.exports = function (phrase) {
+  var counts = {};
+  phrase.split(/\s/).forEach(function (word) {
+    counts[word] = counts.hasOwnProperty(word) ? counts[word] + 1 : 1;
+  });
+  return counts;
+}
