fff785ecaafa4d3ac6b51c887279b402680092e3
Rob Prouse <rob@prouse.org>
2014-11-11 16:17:27 -0500

    Refactoring

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,10 +1,14 @@
 ﻿'use strict';
 
-module.exports.compute = function (dna1, dna2) {
-  var diff = 0;
-  var max = Math.min(dna1.length, dna2.length);
-  for (var i = 0; i < max; i++) {
-    if (dna1[i] !== dna2[i]) diff++;
+var hamming = {
+  compute: function(dna1, dna2) {
+    var diff = 0;
+    var max = Math.min(dna1.length, dna2.length);
+    for (var i = 0; i < max; i++) {
+      if (dna1[i] !== dna2[i]) diff++;
+    }
+    return diff;
   }
-  return diff;
-}
+};
+
+module.exports = hamming;
