22981799ca83c7bafa783bc6a2b9f375c2fefe53
Rob Prouse <rob@prouse.org>
2014-11-11 17:04:44 -0500

    Refactoring

--- coffeescript/anagram/anagram.coffee
+++ coffeescript/anagram/anagram.coffee
@@ -1,26 +1,24 @@
 ﻿class Anagram
 
   # Constructs an Anagram class to find anagrams in lists of words
-  constructor: (anagram) ->    
+  constructor: (anagram) ->
     @anagram = anagram.toLowerCase()
     @sortedAnagram = sort @anagram
-    
+
   # Given a list of words, finds those that are anagrams for
   # the word the class was constructed with.
   match: (words) ->
-    word.toLowerCase() for word in words when word and 
-      word.trim() isnt '' and 
+    word.toLowerCase() for word in words when word and
+      word.trim() isnt '' and
       @isAnagram word.toLowerCase()
-    
-  # Determines if a word is an anagram 
+
+  # Determines if a word is an anagram
   # of the word passed in to the constructor.
   isAnagram: (word) ->
-    word.length is @anagram.length and 
-      word isnt @anagram and 
-      sort(word) is @sortedAnagram
- 
-  # Sorts a string alphabetically   
+    word isnt @anagram and sort(word) is @sortedAnagram
+
+  # Sorts a string alphabetically
   sort = (str) ->
-    str.split('').sort().join("")    
+    str.split('').sort().join('')
 
 module.exports = Anagram
--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -1,35 +1,37 @@
-﻿'use strict';
+﻿(function() {
+  'use strict';
 
-var anagram = function (word) {
-  var sortLetters = function(str) {
-    return str.split('').sort().join('');
-  }
-
-  var isAnagram = function (test) {
-    test = test.toLowerCase();
-    return lower !== test && sorted === sortLetters(test);
-  }
-
-  var lower = word.toLowerCase();
-  var sorted = sortLetters(lower);
-
-  return {
-    matches: function (words) {
-      var anagrams = [];
-
-      if (arguments.length > 1) {
-        words = arguments;
-      }
-
-      for (var i = 0; i < words.length; i++) {
-        if (isAnagram(words[i])) {
-          anagrams.push(words[i]);
-        }
-      }
-
-      return anagrams;
+  var anagram = function(word) {
+    var sortLetters = function(str) {
+      return str.split('').sort().join('');
     }
-  }
-};
 
-module.exports = anagram;
+    var isAnagram = function(test) {
+      test = test.toLowerCase();
+      return lower !== test && sorted === sortLetters(test);
+    }
+
+    var lower = word.toLowerCase();
+    var sorted = sortLetters(lower);
+
+    return {
+      matches: function(words) {
+        var anagrams = [];
+
+        if (arguments.length > 1) {
+          words = arguments;
+        }
+
+        for (var i = 0; i < words.length; i++) {
+          if (isAnagram(words[i])) {
+            anagrams.push(words[i]);
+          }
+        }
+
+        return anagrams;
+      }
+    }
+  };
+
+  module.exports = anagram;
+}).call(this);
