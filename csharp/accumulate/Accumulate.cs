c8d215656612cb1c88892ee233761f01341bfe70
Rob Prouse <rob@prouse.org>
2014-11-18 09:20:00 -0500

    Refactored accumulate

--- csharp/accumulate/Accumulate.cs
+++ csharp/accumulate/Accumulate.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class AccumulateExtensions
 {
     public static IEnumerable<T2> Accumulate<T1, T2>(this IEnumerable<T1> source, Func<T1, T2> operation )
     {
-        foreach (var item in source)
-        {
-            yield return operation(item);
-        }
+        return source.Select( operation );
     }
 }
