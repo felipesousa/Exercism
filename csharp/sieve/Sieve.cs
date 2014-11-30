c49d9d0337672672887d5364240b32d733fc6e34
Rob Prouse <rob@prouse.org>
2014-11-29 19:03:25 -0500

    Prepareing for the sieve problem

---
+++ csharp/sieve/Sieve.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+public class Sieve
+{
+    public Sieve(int i)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public IEnumerable<int> Primes
+    {
+        get { throw new System.NotImplementedException(); }
+    }
+}
