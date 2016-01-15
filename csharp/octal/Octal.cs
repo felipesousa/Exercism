d81199d24d388685c369d3759fe7f448f8fe1908
Rob Prouse <rob@prouse.org>
2016-01-15 09:20:01 -0500

    Failing unit test for Octal

---
+++ csharp/octal/Octal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class Octal
+{
+    public static int ToDecimal(string value)
+    {
+        return 0;
+    }
+}
