548251c0dc38e2dc3d3a8da2abb31471daef5ca2
Rob Prouse <rob@prouse.org>
2016-01-18 16:38:16 -0500

    First octal test passing

--- csharp/octal/Octal.cs
+++ csharp/octal/Octal.cs
@@ -8,6 +8,18 @@ public static class Octal
 {
     public static int ToDecimal(string value)
     {
-        return 0;
+        int result = 0;
+        int len = value.Length;
+        for(int i = 0; i < len; i++)
+        {
+            int oct = ParseOctalValue(value[len - i - 1]);
+            result +=  oct * (int)Math.Pow( 8, i );
+        }
+        return result;
+    }
+
+    static int ParseOctalValue(char c)
+    {
+        return c - '0';
     }
 }
