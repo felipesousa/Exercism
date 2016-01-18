292984ef134286d6a5effcf0e9fd5c757c2519a5
Rob Prouse <rob@prouse.org>
2016-01-18 16:45:44 -0500

    All octal tests passing

--- csharp/octal/Octal.cs
+++ csharp/octal/Octal.cs
@@ -8,18 +8,28 @@ public static class Octal
 {
     public static int ToDecimal(string value)
     {
-        int result = 0;
-        int len = value.Length;
-        for(int i = 0; i < len; i++)
+        try
         {
-            int oct = ParseOctalValue(value[len - i - 1]);
-            result +=  oct * (int)Math.Pow( 8, i );
+            int result = 0;
+            int len = value.Length;
+            for(int i = 0; i < len; i++)
+            {
+                int oct = ParseOctalValue(value[len - i - 1]);
+                result += oct * (int)Math.Pow(8, i);
+            }
+            return result;
         }
-        return result;
+        catch(ArgumentException)
+        {
+            return 0;
+        }
     }
 
     static int ParseOctalValue(char c)
     {
+        if(c < '0' || c > '7')
+            throw new ArgumentException("Must be 0 through 7", nameof(c));
+
         return c - '0';
     }
 }
--- csharp/octal/OctalTest.cs
+++ csharp/octal/OctalTest.cs
@@ -5,29 +5,28 @@ public class OctalTest
 {
     // change Ignore to false to run test case or just remove 'Ignore = "Todo"'
     [TestCase("1", ExpectedResult = 1)]
-    [TestCase("10", ExpectedResult = 8, Ignore = "Todo")]
-    [TestCase("17", ExpectedResult = 15, Ignore = "Todo")]
-    [TestCase("11", ExpectedResult = 9, Ignore = "Todo")]
-    [TestCase("130", ExpectedResult = 88, Ignore = "Todo")]
-    [TestCase("2047", ExpectedResult = 1063, Ignore = "Todo")]
-    [TestCase("7777", ExpectedResult = 4095, Ignore = "Todo")]
-    [TestCase("1234567", ExpectedResult = 342391, Ignore = "Todo")]
+    [TestCase("10", ExpectedResult = 8)]
+    [TestCase("17", ExpectedResult = 15)]
+    [TestCase("11", ExpectedResult = 9)]
+    [TestCase("130", ExpectedResult = 88)]
+    [TestCase("2047", ExpectedResult = 1063)]
+    [TestCase("7777", ExpectedResult = 4095)]
+    [TestCase("1234567", ExpectedResult = 342391)]
     public int Octal_converts_to_decimal(string value)
     {
         return Octal.ToDecimal(value);
     }
 
-    [TestCase("carrot", Ignore = "Todo")]
-    [TestCase("8", Ignore = "Todo")]
-    [TestCase("9", Ignore = "Todo")]
-    [TestCase("6789", Ignore = "Todo")]
-    [TestCase("abc1z", Ignore = "Todo")]
+    [TestCase("carrot")]
+    [TestCase("8")]
+    [TestCase("9")]
+    [TestCase("6789")]
+    [TestCase("abc1z")]
     public void Invalid_octal_is_decimal_0(string invalidValue)
     {
         Assert.That(Octal.ToDecimal(invalidValue), Is.EqualTo(0));
     }
 
-    [Ignore("Todo")]
     [Test]
     public void Octal_can_convert_formatted_strings()
     {
