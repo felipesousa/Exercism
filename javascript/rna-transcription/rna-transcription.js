be82396c3904886f1370471380e21a19f13fdacd
Rob Prouse <rob@prouse.org>
2016-01-24 20:09:44 -0500

    RNA exercise in Javascript

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,7 +1,14 @@
 ﻿(function () {
   module.exports = function (dna) {
-    this.toRna = function (nucleotide) {
-      return 'G';
+    this.toRna = function (dna) {
+      var rna = '';
+      for(var i=0; i<dna.length; i++) {
+        var d = dna[i].toUpperCase();
+        rna += complements[d] || '';
+      }
+      return rna;
     }
+
+    var complements = { G: 'C', C: 'G', T: 'A',  A: 'U' };
   };
 })();
