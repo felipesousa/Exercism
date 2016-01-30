3f7d24bec1b6b417f23db531e529c1d2e9238fda
Rob Prouse <rob@prouse.org>
2016-01-30 16:18:18 -0500

    Switch Gigasecond problem to a different format

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,7 +1,9 @@
 ﻿(function () {
-  module.exports = function (birthday) {
+  var Gigasecond = function (birthday) {
     this.date = function () {
       return new Date(birthday.valueOf() + 1E12);
     };
-  }
+  };
+
+  module.exports = Gigasecond;
 })();
