7266f3b9f1f8e7bf62c5a2037c068582c8489949
Rob Prouse <rob@prouse.org>
2017-11-07 16:53:21 -0500

    Initial solution to the Callatz Conjecture problem

--- csharp/collatz-conjecture/CollatzConjecture.cs
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -4,6 +4,17 @@ public static class CollatzConjecture
 {
     public static int Steps(int number)
     {
-        throw new NotImplementedException("You need to implement this function");
+        if(number < 1) throw new ArgumentException(nameof(number));
+
+        var steps = 0;
+        while(number != 1)
+        {
+            steps++;
+            if(number % 2 == 0)
+                number /= 2;
+            else
+                number = number * 3 + 1;
+        }
+        return steps;
     }
 }
