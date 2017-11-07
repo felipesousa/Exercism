ebafa70de35cf20ecb647f682052c12f0c6fe7eb
Rob Prouse <rob@prouse.org>
2017-11-07 16:55:46 -0500

    A slightly shorter version of the Collatz problem

--- csharp/collatz-conjecture/CollatzConjecture.cs
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -10,10 +10,7 @@ public static class CollatzConjecture
         while(number != 1)
         {
             steps++;
-            if(number % 2 == 0)
-                number /= 2;
-            else
-                number = number * 3 + 1;
+            number = number % 2 == 0 ? number / 2 : number * 3 + 1;
         }
         return steps;
     }
