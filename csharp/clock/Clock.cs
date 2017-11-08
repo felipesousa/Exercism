58bc9197d7df1a5b50fcf7631e78079cb91c72f7
Rob Prouse <rob@prouse.org>
2017-11-08 09:28:53 -0500

    Implemented addition and substraction

--- csharp/clock/Clock.cs
+++ csharp/clock/Clock.cs
@@ -24,15 +24,11 @@ public struct Clock
 
     public int Minutes { get; private set; }
 
-    public Clock Add(int minutesToAdd)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public Clock Add(int minutesToAdd) =>
+        new Clock(Hours, Minutes + minutesToAdd);
 
-    public Clock Subtract(int minutesToSubtract)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public Clock Subtract(int minutesToSubtract) =>
+        new Clock(Hours, Minutes - minutesToSubtract);
 
     public override string ToString() => $"{Hours:00}:{Minutes:00}";
 }
