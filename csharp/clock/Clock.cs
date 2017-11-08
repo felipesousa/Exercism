9976a75c132206205a9e04e288c45c58319a0779
Rob Prouse <rob@prouse.org>
2017-11-08 09:45:11 -0500

    Implemented equality

--- csharp/clock/Clock.cs
+++ csharp/clock/Clock.cs
@@ -1,6 +1,6 @@
 using System;
 
-public struct Clock
+public struct Clock : IEquatable<Clock>
 {
     const int MINUTES_IN_HOUR = 60;
     const int HOURS_IN_DAY = 24;
@@ -8,14 +8,7 @@ public struct Clock
 
     public Clock(int hours, int minutes)
     {
-        var totalMinutes = hours * MINUTES_IN_HOUR + minutes;
-
-        // Roll over for the day
-        totalMinutes = totalMinutes % MINUTES_IN_DAY;
-
-        // Roll over for the previous day
-        if(totalMinutes < 0) totalMinutes += MINUTES_IN_DAY;
-
+        var totalMinutes = TotalMinutes(hours, minutes);
         Hours = totalMinutes / MINUTES_IN_HOUR;
         Minutes = totalMinutes - (Hours * MINUTES_IN_HOUR);
     }
@@ -31,4 +24,29 @@ public struct Clock
         new Clock(Hours, Minutes - minutesToSubtract);
 
     public override string ToString() => $"{Hours:00}:{Minutes:00}";
+
+    public override bool Equals(object obj)
+    {
+        if(!(obj is Clock)) return false;
+        return Equals((Clock)obj);
+    }
+
+    public override int GetHashCode() =>
+        TotalMinutes(Hours, Minutes).GetHashCode();
+
+    static int TotalMinutes(int hours, int minutes)
+    {
+        var totalMinutes = hours * MINUTES_IN_HOUR + minutes;
+
+        // Roll over for the day
+        totalMinutes = totalMinutes % MINUTES_IN_DAY;
+
+        // Roll over for the previous day
+        if(totalMinutes < 0) totalMinutes += MINUTES_IN_DAY;
+
+        return totalMinutes;
+    }
+
+    public bool Equals(Clock other) =>
+        TotalMinutes(Hours, Minutes) == TotalMinutes(other.Hours, other.Minutes);
 }
