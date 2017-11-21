397adb951bd4240a476ad81e691ae7cf8500db15
Rob Prouse <rob.prouse@bentley.com>
2017-11-20 19:40:47 -0500

    Solved the Bob problem in TypeScript

--- typescript/bob/bob.ts
+++ typescript/bob/bob.ts
@@ -1,6 +1,13 @@
 class Bob {
-    hey( /* Parameters go here */ ) {
-        // Your code here
+    hey( say: string ): string {
+        if (say.replace(/\s/g, '') === '') {
+            return 'Fine. Be that way!'
+        } else if (say.match(/[a-z]/i) && say.toUpperCase() === say) {
+            return 'Whoa, chill out!'
+        } else if (say.endsWith('?')) {
+            return 'Sure.'
+        }
+        return 'Whatever.'
     }
 }
 
